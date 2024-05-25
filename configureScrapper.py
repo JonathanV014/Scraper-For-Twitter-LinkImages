80a12a6d825b7f8e9484bb789a5d5091c351954c
Jonathan Vizcaino <jevizcaino@unimagdalena.edu.co>
2024-05-25 16:20:59 -0500

    Se creoó un archivo para manipular la configuración de tu scrapper

--- configureScrapper.py
+++ configureScrapper.py
@@ -1,18 +1,21 @@
 import os
 from datetime import datetime
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
 
 
 
-STARTDATE = datetime(2023,5,8) #Replace for your limits
+#Replace for your limits
+STARTDATE = datetime(2023,5,8) 
 ENDDATE = datetime(2023,12,31)
 
 SCROLLTIME = 500 #Seconds
 DISTANCE = 250 #Pixels
 
+#where you are going to save your csv
 DIRSAVECSV = "imgs/"
 NAMECSV = "urlImagenesAccidentes.csv"
 
@@ -20,24 +23,8 @@ NAMECSV = "urlImagenesAccidentes.csv"
 SEARCHEQUATION = "((accidente or siniestro or colision or choque) and (transito or vehicular or automovilistico)))"
 
 
+
 #Read from your .env twitter username and password 
 USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
 
-
-
-SEARCHEQUATION = SEARCHEQUATION.replace(" ", "%20")
-SEARCHEQUATION = SEARCHEQUATION.replace("since:", "since%3A")
-SEARCHEQUATION = SEARCHEQUATION.replace("until:", "until%3A")
-
-
-
-COMOQUEDA = f"https://x.com/search?q=((accidente%20or%20siniestro%20or%20colision%20or%20choque)%20and%20(transito%20or%20vehicular%20or%20automovilistico)))%20since%3A2023-05-08%20until%3A2023-05-09&src=spelling_expansion_revert_click"
-
-falq = f"https://x.com/search?q={SEARCHEQUATION}&src=spelling_expansion_revert_click" 
-print(falq)
-print(len(falq))
-print(len(COMOQUEDA))
-
-print(falq == COMOQUEDA)
-# # print(SEARCHEQUATION)
