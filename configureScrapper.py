b6e8d81d6b767e04f49cf20739f061322ddde135
Jonathan Vizcaino <jevizcaino@unimagdalena.edu.co>
2024-05-25 17:25:09 -0500

    Variables definidas por defecto

---
+++ configureScrapper.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#Replace for your limits
+STARTDATE = datetime(2023,8,22) 
+ENDDATE = datetime(2023,12,31)
+
+SCROLLTIME = 600 #Seconds
+DISTANCE = 250 #Pixels
+
+#where you are going to save your csv
+DIRSAVECSV = "imgs/"
+NAMECSV = "urlImagenesAccidentes.csv"
+
+#Replace for you search equation
+SEARCHEQUATION = "((accidente or siniestro or colision or choque) and (transito or vehicular or automovilistico)))"
+
+#Read from your .env twitter username and password 
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
