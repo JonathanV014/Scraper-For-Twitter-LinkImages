13243264f52ca7a61cfba7a31de789e1e26aae88
Jonathan Vizcaino <jevizcaino@unimagdalena.edu.co>
2024-05-25 16:23:23 -0500

    Se creó este archivo para crear tu scrapper y ejecutarlo

---
+++ runScrapper.py
@@ -0,0 +1,10 @@
+from configureScrapper import *
+from test import TwitterScrapper
+
+
+
+#Create object TwitterScrapper and run
+myScrapper = TwitterScrapper(startDate=STARTDATE, endDate=ENDDATE, scrollTime=SCROLLTIME, distance=DISTANCE,dirSaveCsv=DIRSAVECSV, nameCsv=NAMECSV,searchEquation=SEARCHEQUATION, user=USER, password=PASSWORD)
+
+myScrapper.startScrapper()
+
